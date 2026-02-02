d87d67f4f782cdc761cc03e76e8f4033e75c4a93
Varshik <varshikjain2002@gmail.com>
2026-02-03 00:16:42 +0530

    changes

--- apps/auth-service/src/auth-service.module.ts
+++ apps/auth-service/src/auth-service.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AuthServiceController } from './auth-service.controller';
 import { AuthServiceService } from './auth-service.service';
 import { KafkaModule } from '@app/kafka';
+import { DatabaseModule } from '@app/database/database.module';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [KafkaModule.register('auth-service-group')],
+  imports: [KafkaModule.register('auth-service-group'), DatabaseModule, 
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'secret',
+      signOptions: { expiresIn: '1d' },
+    }), 
+    PassportModule],
   controllers: [AuthServiceController],
-  providers: [AuthServiceService],
+  providers: [AuthServiceService, JwtStrategy],
 })
 export class AuthServiceModule {}
