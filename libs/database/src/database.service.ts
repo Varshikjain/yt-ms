6680bab62a96806a044c7e157a0090951c554fff
Varshik <varshikjain2002@gmail.com>
2026-02-14 21:49:02 +0530

    fix(database-service): correct typo in connection string and commented code

--- libs/database/src/database.service.ts
+++ libs/database/src/database.service.ts
@@ -9,10 +9,14 @@ export class DatabaseService {
     private pool:Pool;
     public db: NodePgDatabase<typeof schema>
 
-    //'postgresql://eventflow:eventflow_password@locahost:5432/eventflowdb?schema=public
+    //'postgresql://eventflow:eventflow_password@localhost:5432/eventflowdb?schema=public
+    //{
+    // "message": "User registered successfully",
+    // "userId": "7b6a9159-4867-45a9-baf1-36e28fc0aaa9"
+// }
     constructor() {
         // const connectionString = process.env.DATABASE_URL!;
-        const connectionString = 'postgresql://eventflow:eventflow_password@locahost:5432/eventflowdb?schema=public';
+        const connectionString = 'postgresql://eventflow:eventflow_password@localhost:5432/eventflowdb?schema=public';
 
         this.pool = new Pool({ connectionString});
         this.db = drizzle(this.pool, { schema });
