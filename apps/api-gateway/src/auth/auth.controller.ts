f5403037fb0dc06ec464d2bf33922f556fdfbf85
Varshik <varshikjain2002@gmail.com>
2026-02-18 22:04:30 +0530

    feat(auth-service): implement AuthController with registration, login, and profile endpoints

--- apps/api-gateway/src/auth/auth.controller.ts
+++ apps/api-gateway/src/auth/auth.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Headers } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from '@app/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService){}
+
+    @Post('register')
+    register(@Body() RegisterDto: RegisterDto) {
+        return this.authService.register(RegisterDto);
+    }
+
+    @Post('login')
+    login(@Body() LoginDto: any) {
+        return this.authService.login(LoginDto);
+    }
+
+    @Post('profile')
+    profile(@Headers('Authorization') authorization: string) {
+        return this.authService.getProfile(authorization);
+    }
+
+}
--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SERVICE_PORTS } from '@app/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true, // Strip properties that do not have any decorators
+        forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are present
+        transform: true, // Automatically transform payloads to be objects typed according to their DTO classes
+      }),
+    )
+
+
+  await app.listen(SERVICE_PORTS['api-gateway']);
 }
 bootstrap();
