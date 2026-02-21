1b1540345e725e512b7cec5f465408e4824246e3
Varshik <varshikjain2002@gmail.com>
2026-02-21 11:30:32 +0530

    feat(events-service): integrate Kafka module and add global validation pipes

--- apps/events-service/src/events-service.module.ts
+++ apps/events-service/src/events-service.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EventsServiceController } from './events-service.controller';
 import { EventsServiceService } from './events-service.service';
+import { DatabaseModule } from '@app/database/database.module';
+import { KafkaModule } from '@app/kafka';
 
 @Module({
-  imports: [],
+  imports: [KafkaModule.register('events-service-group'), DatabaseModule],
   controllers: [EventsServiceController],
   providers: [EventsServiceService],
 })
--- apps/events-service/src/main.ts
+++ apps/events-service/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { EventsServiceModule } from './events-service.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SERVICE_PORTS } from '@app/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(EventsServiceModule);
-  await app.listen(process.env.port ?? 3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Strip properties that do not have any decorators
+      forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are present
+      transform: true, // Automatically transform payloads to be objects typed according to their DTO classes
+    }),
+  );
+  await app.listen(SERVICE_PORTS['events-service']);
+  console.log(`Events Service is running on port ${SERVICE_PORTS['events-service']}`);
 }
 bootstrap();
